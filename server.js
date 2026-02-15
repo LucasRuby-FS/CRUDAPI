70673a47f8f00dc1a63d33ca0891014531925fb7
LucasRuby-FS <rklucas@student.fullsail.edu>
2026-02-14 23:30:54 -0500

    Added API starting files.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+const bearRouter = require("./routes/bears");
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+
+db.once(`open`, () => console.log("database connection worked"));
+
+app.use(express.json());
+
+app.use("/bears", bearRouter);
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
