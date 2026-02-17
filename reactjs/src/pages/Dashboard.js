956665569d09e897292dc6d43bc34685eb2ffd79
LucasRuby-FS <rklucas@student.fullsail.edu>
2026-02-17 15:46:45 -0500

    Adding more.

--- reactjs/src/pages/Dashboard.js
+++ reactjs/src/pages/Dashboard.js
@@ -1,3 +1,13 @@
+//All red lines throughout this page.
+//Routes do not work.
+//React Router errors.
+//I cannot figure out how to connect the reactjs and api.
+//Build has made problems, I think to the routing and trying to use REST on VSCode to stimulate the requests.
+//I am not confident on the package.jsons being made correctly. As well as the server.js and App.js.
+//The localhost:8000 can only be accepted by adding /api/v1/bears example:
+//localhost:8000/api/v1/bears
+//I did have an issue trying to change my folder name from "API" to "api".
+//I am not sure if the files are correct in the api folder.
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 function Dashboard() {
@@ -52,11 +62,3 @@ function Dashboard() {
 }
 
 export default Dashboard;
-//All red lines throughout this page.
-//Routes do not work.
-//React Router errors.
-//I cannot figure out how to connect the reactjs and api. The localhost only goes to 3000, and I need to use 8000.
-//Build has made problems, I think to the routing and trying to use REST on VSCode to stimulate the requests.
-//I am not confident on the package.jsons being made correctly. As well as the server.js and App.js.
-//The localhost:8000 can only be accepted by adding /api/v1/bears example:
-//localhost:8000/api/v1/bears
